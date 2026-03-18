9fa49dfac5dc7adf71e7721f1a0662383d93f688
Loreashford <vinishananya@gmail.com>
2026-03-18 10:57:53 +0530

    uc2

---
+++ uc3.java
@@ -0,0 +1,14 @@
+class uc3
+{
+  public static void main(String args[])
+  {
+  Sysytem.out.println(String.join("  ", " *** "," "," *** "," ","** **"," ","**"));
+  System.out.println(String.join("  ","** **"," ","** **"," ","** **"," ","**"));
+  sysytem.out.println(String.join("  ","** **"," ","** **"," ","**** "," ","**"));
+  System.out.println(String.join(" ","** **"," ","** **"," ","**  "," ","***"));
+  System.out.println(String.join("  ","** **"," ","** **"," ","**  "," ","**"));
+  System.out.println(String.join("  ","** **"," ","** **"," ","**   "," "," ****"));
+  system.out.println(String.join("  "," *** "," "," ***"," "," **   "," "," ****"));
+  }
+}
+  
